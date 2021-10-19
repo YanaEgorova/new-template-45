0d6ce5b1d7b360042c41b3e384c5e74cfe3b27be
Yana <yanaegorova69.3@gmail.com>
2021-10-19 22:57:24 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,292 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Aquarium Decoration',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'This aquarium decoration features a large grey castle perched upon a rocky hill. The cone shaped roof, archways, and stone details give a realistic feel.',
+            },
+            {
+                text: 'This castle provides visual interest for you and your fish. Holes provide hideaways for your fish to rest and passageways to reduce boredom.',
+            },
+            {
+                text: 'This aquarium ornament is 8 inches wide by 10.2 inches high by 6.2 inches deep. Castle is doubled sided and can be placed anywhere in your fish tank.',
+            },
+            {
+                text: 'Made of safe durable resin, this aquarium decoration is hand painted with detail and is safe for use in both freshwater and saltwater aquariums.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Pet Food Container',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Airtight seal keep out moisture, humidity and pests, "Positive-snap" latche so you know when the lid is closed.',
+            },
+            {
+                text: 'Tapered shape from top to bottom; easy to clean.',
+            },
+            {
+                text: 'Holds 42-lbs dog food, 35-lbs cat food, 63-lbs of bird seed, 69-lbs cat litter.',
+            },
+            {
+                text: 'Made in the USA; FDA-compliant and BPA-free material.',
+            },
+            {
+                text: 'Dimensions: 18.5 L x 13.6 W x 22.9 H inches.',
+            },
+            {
+                text: 'Casters are included for mobility.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png', './img/img2-6.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Pet Travel Carrier',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                text: '2-door model allows for easy loading of pets.',
+            },
+            {
+                text: 'Plenty of ventilation on sides, top and back of crate.',
+            },
+            {
+                text: 'Included screws can be used to attach top and bottom for additional security.',
+            },
+            {
+                text: 'Spring-loaded from latch for smooth opening.',
+            },
+            {
+                text: 'Top door features handle and latches to open to the left or right for convenience.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Cat Condo',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Elevated cat hammock with dual scratching post pillars; durable, long-lasting construction.',
+            },
+            {
+                text: 'Plush hammock provides a comfortable space for your cat to relax.',
+            },
+            {
+                text: 'Helps keep your cat from damaging carpets, furniture, curtains, and more.',
+            },
+            {
+                text: 'Natural jute fiber scratching posts help keep nails healthy.',
+            },
+            {
+                text: 'Neutral color tones fit in with your home’s existing décor.',
+            },
+            {
+                text: 'Dimensions: 15.75 x 15.75 x 19.69 inches; 8.95 pounds.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Pet Gate',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Block doorways and hallways to keep your pet from entering forbidden areas.',
+            },
+            {
+                text: 'Durable all-steel construction.',
+            },
+            {
+                text: 'Fits opening from 38 to 42.5 inches.',
+            },
+            {
+                text: 'Pressure-mounted for easy installation and takedown.',
+            },
+            {
+                text: 'Can fit opening up to 198.5 inches with optional extensions (sold separately).',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Pet Steps',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Relieves Stress',
+                text: 'Our stairs are the perfect accessory to help pets reach that high bed or couch! Available in 3-step, 4-step, and 5-step configurations to easily match your furniture’s height.',
+            },
+            {
+                title: 'Supports Pet Health',
+                text: 'Made with mattress-grade cushioning to relieve pressure on paws and joints.',
+            },
+            {
+                title: 'Save Time',
+                text: 'Each stair cover is removable and machine-washable! No more cleaning off dirt and fur by hand.',
+            },
+            {
+                title: 'Decorate Your Home',
+                text: 'Available in 4 stylish patterns and 8 colors to match the personality of your home!',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.jpg'],
+    },
+    {
+        id: 'product-7',
+        name: 'Bike Pet Carrier',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                title: 'Strong',
+                text: 'Travel Bike Basket is the perfect solution to bicycling with your dog and holds up to 20 lbs.',
+            },
+            {
+                title: 'All Inclusive',
+                text: 'Includes basket and front bicycle mount (hood and rear bike mount are also available separately).',
+            },
+            {
+                title: 'Storage',
+                text: 'Zipper pocket for storing leashes and treats, mesh pocket for holding water bottles.',
+            },
+            {
+                title: 'Security',
+                text: 'Dual leash design inside the basket keeps your pet safe and secure in the bike basket.',
+            },
+            {
+                title: 'Washable',
+                text: 'Removable fleece liner in the basket is machine washable for easy care.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.png', './img/img7-5.png', './img/img7-6.png', './img/img7-7.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Grooming Table',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Size: 30"L x 18" W x 32" H Color : Black.',
+            },
+            {
+                text: 'Table features strong goal post style legs with rubber capped feet, ensuring the table does not slip. Includes sturdy aluminum adjustable grooming arm with clamp, extends to 40" high.',
+            },
+            {
+                text: 'Goal post style legs allow for easy fold up during transport and also add extra stability.',
+            },
+            {
+                text: 'Rust-proof materials such as the deluxe aluminum edging along the table and the advanced coating above the table ensures a long lasting product.',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: 'Pet Coat',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'Winter fleece jumper with integrated harness.',
+            },
+            {
+                text: 'Checkered pattern and solid schemed.',
+            },
+            {
+                text: 'Decorated with waste dispenser on the back.',
+            },
+            {
+                text: 'Size: Large : 12.6" Neck, 18.9" Chest, 12.2" Product Length.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Pet Ramp',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'High-visibility reflective material aids in guiding your pet up and down the ramp.',
+            },
+            {
+                text: 'Side safety reflectors allow for maximum visibility when the ramp is in use.',
+            },
+            {
+                text: 'Slip-resistant surface provides sure footing and rubber grippers on bottom help keep ramp steady.',
+            },
+            {
+                text: 'Easy fold for compact storage and built-in handle for portability.',
+            },
+            {
+                text: 'Dimensions: 71"L x 16"W x 4"H; suitable for pets up to 200 pounds.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Pet Hair Dryer',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Powerful Blow Force & Stepless Adjustable Speed',
+                text: 'Dog dryer blower professional equipped with a powerful and stable motor. Peak Power: 3.8HP(2800W). Voltage: 110V-120V. High velocity dryer for dogs and cats. Stepless Adjustable Speed. Whether it\'s a giant dog or mini cat, it can quickly dry. You can choose different wind level with your pet size. Give your lovely pet a thorough dry after shower.',
+            },
+            {
+                title: 'Quickly Dry',
+                text: 'Our product can work at a safe temperature all the time! It has a heater with protector to switch for cold or warm. Temperature Range：85℉-140℉. Except pet, you also can use it to blow your car or bicycle.',
+            },
+            {
+                title: 'Reduce Noise & Heat Insulation',
+                text: 'Thickened metal case can reduce nose effective. Heat insulation can achieve the effect of noise reduction and shock absorption. Intimate design protects your pet from fright.',
+            },
+            {
+                title: '4 Nozzles & Flexible Hose',
+                text: 'Use different nozzles, you can make different shapes for your lovely pets. You can use the different nozzles for different part of their body.',
+            },
+            {
+                title: 'Package & Superior Service',
+                text: 'Pay attention to waterproof. Avoid pets to bite the hose and power cable. Please do not hesitate to contact us. We Provide replacement parts as Carbon Brush, Hose, Nozzles, Filter and so on. If the item you received is defective in any way, please email us for replacement or any other question.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Air Pump',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                text: 'Widely used to provide oxygen in aquariums, fish farms and hydroponic systems.',
+            },
+            {
+                text: 'Includes chrome air manifold, ranging from six to twelve outlets. Volume (CF): 0.5037.',
+            },
+            {
+                text: 'Cylinders and pistons are made of premium materials, making the pump strong and durable.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png','./img/img12-3.png','./img/img12-4.png','./img/img12-5.png'],
+    },
+]
